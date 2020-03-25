f91045b75c934d7b9b7eb1e8f02681da66d682ef
Victor <demebtor0@gmail.com>
2020-03-25 14:41:08 +0300

    '

---
+++ WebApplication112/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication112.Models
+{
+    public class Book
+    {
+        public int Price { get; set; }
+
+    }
+}
